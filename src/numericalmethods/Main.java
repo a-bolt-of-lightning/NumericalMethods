15991b0c0bc710d50b19310d0376a592b059c80f
a_bolt_of_lightning <46372775+a-bolt-of-lightning@users.noreply.github.com>
2019-07-13 19:37:03 +0430

    add help pdf

--- src/numericalmethods/Main.java
+++ src/numericalmethods/Main.java
@@ -1,8 +1,16 @@
 package numericalmethods;
 
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * Driver class used in the implementation of numerical methods
@@ -20,14 +28,49 @@ public class Main extends Application {
         //y'(x)=-2y(x)+cos(4x) with y(0)=3
         //y = 0.1*cos(4*x)+0.2*sin(4*x)+2.9*exp(-2*x);
 
+        VBox root = new VBox();
+
+        MenuBar menuBar = new MenuBar();
+
+        setMenuBar(menuBar);
+
         NumericalVisualization graph = new NumericalVisualization(0, 3);
 
-        Scene scene =  new Scene(graph, 1000, 650);
+
+        root.getChildren().addAll(menuBar, graph);
+
+        Scene scene =  new Scene(root, 1000, 670);
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Numerical Method");
         primaryStage.show();
+
+    }
+
+    private static void setMenuBar(MenuBar menuBar){
+
+
+        Menu helpMenu = new Menu("Help");
+        menuBar.getMenus().add(helpMenu);
+
+        MenuItem helpItem = new MenuItem("Help");
+        MenuItem aboutItem = new MenuItem("About");
+        helpMenu.getItems().addAll(helpItem, aboutItem);
+
+        aboutItem.setOnAction(e->{
+            Pane aboutPage = new Pane();
+            setAboutPage(aboutPage);
+
+            Stage secondStage =
+                    new Stage();
+            Scene secondScene = new Scene(aboutPage, 400, 200);
+            secondStage.setScene(secondScene);
+            secondStage.show();
+        });
+    }
+
+    private static void setAboutPage(Pane aboutPage){
 
     }
 
